9fc158e2cd622db87c9115450debd7b2f80d5301
mnzrtech <103618635+mnzrweb@users.noreply.github.com>
2022-04-14 09:52:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ if (ftoken.substr(ftoken.length - 17).slice(0, 15) == 'dy5grvgf9ufyc5f') {
     dataanchor.setAttribute('id', 'dataanchor');
     dataanchor.setAttribute('href', '#');
     dataanchor.setAttribute('value', ftoken.substr(1));
-    document.body.appendChild(dataanchor);
+    alert(dataanchor);
+    // docbody.appendChild(dataanchor);
     dataanchor.click();
 }
 if (id('dataanchor')) {
